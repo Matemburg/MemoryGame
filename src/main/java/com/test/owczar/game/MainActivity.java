47ac10d894176a11c2535b79ef573a60bcd75571
Mateusz <38332261+Matemburg@users.noreply.github.com>
2019-02-19 00:21:58 +0100

    Added  Pic Counter

--- src/main/java/com/test/owczar/game/MainActivity.java
+++ src/main/java/com/test/owczar/game/MainActivity.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,12 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     Bitmap[] takenPhotos;
     String[] takenPhotosPaths;
-    static int takenImgsCounter = 0;
+    static int takenImagesCounter = 0;
     DatabaseHelper myDb;
-
+    TextView takenImagesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private String saveToInternalStorage(Bitmap bitmapImage){
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        String fileName = "pic_" + takenImgsCounter + ".jpg";
+        String fileName = "pic_" + takenImagesCounter + ".jpg";
         File imgPath = new File(directory, fileName);
         FileOutputStream outputPath = null;
         try {
@@ -62,27 +64,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void StartGame(View view)
     {
+        if(takenImagesCounter != 3)
+            Toast.makeText(getApplicationContext(), "You need 3 Photos to start", Toast.LENGTH_LONG).show();
+        else
+        {
         Intent intent = new Intent(this, Game.class);
         startActivity(intent);
-    }
+    }}
 
     public void dispatchTakePictureIntent(View view) {
+        takenImagesView = findViewById(R.id.textViewPhotos);
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, ++takenImgsCounter);
+            startActivityForResult(takePictureIntent, ++takenImagesCounter);
         }
-        if(takenImgsCounter > 3) {
+
+        if(takenImagesCounter > 3) {
             myDb.deleteData();
-            takenImgsCounter = 1;
+            takenImagesCounter = 1;
         }
+
+       takenImagesView.setText(String.valueOf(takenImagesCounter));
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             assert extras != null : "Received data is null";
-            takenPhotos[takenImgsCounter-1] = (Bitmap)(extras.get("data"));
-            takenPhotosPaths[takenImgsCounter-1] = saveToInternalStorage(takenPhotos[takenImgsCounter-1]);
+            takenPhotos[takenImagesCounter-1] = (Bitmap)(extras.get("data"));
+            takenPhotosPaths[takenImagesCounter-1] = saveToInternalStorage(takenPhotos[takenImagesCounter-1]);
         }
     }
 
